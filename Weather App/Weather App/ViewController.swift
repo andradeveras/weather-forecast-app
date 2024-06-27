d71bfd30f4d962d49e72e51d9410ffed70b1313f
Leonardo Veras <andradeveras96@gmail.com>
2024-06-27 16:51:01 -0300

    Starting the Weather API

--- Weather App/Weather App/ViewController.swift
+++ Weather App/Weather App/ViewController.swift
@@ -166,11 +166,17 @@ class ViewController: UIViewController {
         return tableView
     }()
     
+    private let service = Service()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         setupView()
+        
+        service.fecthData(city: City(lat:  "-23.6814346", lon: "-46.9249599", name: "São Paulo")) { message in
+             print(message)
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
